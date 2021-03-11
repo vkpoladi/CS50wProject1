36659c529b79c89f21f432cde60242e2add633a1
vkpoladi <vkpoladi@seas.upenn.edu>
2021-03-11 09:53:44 -0600

    Search Requirement Complete

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -24,3 +24,23 @@ def entry(request, title):
             "title":markdowner.convert(md_entry),
             "heading":title
     })
+
+def search(request):
+    title = request.GET["q"]
+    md_entry = util.get_entry(title)
+    if util.get_entry(title) != None:
+        return render(request, "encyclopedia/entries.html", {
+            "title":markdowner.convert(md_entry),
+            "heading":title
+        })
+    else:
+        entries = util.list_entries()
+        similar_entries = []
+        for entry in entries:
+            if title in entry:
+                similar_entries.append(entry)
+
+        return render(request, "encyclopedia/search.html", {
+            "search_word":title,
+            "similar_entries": similar_entries 
+        })
