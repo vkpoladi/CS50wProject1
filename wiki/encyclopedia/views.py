5adec0355168c2d302eccf6f413926b9a44df5f0
vkpoladi <vkpoladi@seas.upenn.edu>
2021-03-10 18:47:15 -0600

    Implemented markdown2

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
+
+#import installed Markdown2 and save in variable
+#this will be used to convert markdown entry files to html
+#pip install markdown2
+from markdown2 import Markdown
+markdowner = Markdown()
+
 
 # Create your views here.
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    md_entry = util.get_entry(title)
+    return render(request, "encyclopedia/entries.html", {
+        "title":markdowner.convert(md_entry)
     })
